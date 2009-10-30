ca380a660ca2f439ea4a7313cfa8dca7a04232b6
biern <biern.m@gmail.com>
2009-10-30 15:39:38 +0100

    Removed pickle loading / dump, moved language data to 'data' module instead.
    All language data is and probably will be only plain text so using pickle is unnecessary.

--- src/languages.py
+++ src/languages.py
@@ -5,11 +5,12 @@ Created on 2009-10-29
 '''
 #list format: (cc, name, bmp, wx.ID)
 
-import pickle
 import os
 import wx
 from wx.lib import langlistctrl
 from wx.tools import img2py
+
+import data
 
 FILENAME = "languages.pickle"
 INFO_FILENAME = "languages_info.txt"
@@ -17,13 +18,6 @@ INFO_FILENAME = "languages_info.txt"
 def flag_for(country_code):
     cnt = country_code.split('_')[1].upper()
     return wx.ArtProvider.GetBitmap('wx.ART_'+cnt, wx.ART_OTHER, (16,11))
-
-def load_languages():
-    if os.path.exists(FILENAME):
-        file = open(FILENAME)
-        return pickle.load(file)
-    else:
-        return build_languages()
 
 def build_languages():
     result = {}
@@ -33,17 +27,28 @@ def build_languages():
         #Can't save wx bmps in pickle
         result[country_mappings[lid]] = name
     
-    file = open(FILENAME, 'w')
-    pickle.dump(result, file)
-    generate_languages_info_file(result)
     return result
 
+def get_languages():
+    return data.LANGUAGE_CODES
 
 def generate_languages_info_file(data):
     file = open(INFO_FILENAME, 'w')
     lines = []
     for k in sorted(data.keys()):
-        lines.append("{0:6} : {1}\n".format(k, data[k]))
+        lines.append("{0:6} : {1},\n".format('"'+k+'"', '"'+data[k]+'"'))
         
     file.writelines(lines)
     file.close()
+
+def parse_country_code(code):
+    code = code.strip()
+    try:
+        code = data.LANGUAGE_CODES_MAPPINGS[code]
+    except KeyError:
+        pass
+        
+    if '_' not in code:
+        code += '_'+code.upper()
+        
+    return code
