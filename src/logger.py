c17f763ab485f8e5a671cb6ff47a138259eb399b
biern <biern.m@gmail.com>
2010-03-01 19:08:57 +0100

    Added log_call function

--- src/logger.py
+++ src/logger.py
@@ -7,8 +7,23 @@ Created on 2010-03-01
 import logging
 import logging.config
 
-from main import path_to
-
 def setup_logging():
+    from main import path_to
     logging.config.fileConfig(path_to("config/logging.conf"))
 
+def log_call(log, dummy=False):
+    """
+    Returns decorator logging function name when it's called.
+    If dummy is True then created decorator has no effect on a function.
+    """
+    def decorator(callable):
+        def func(*args, **kwargs):
+            log.debug(callable.__name__)
+            return callable(*args, **kwargs)
+        
+        return func
+    
+    if not dummy:
+        return decorator
+    else:
+        return lambda x: x
